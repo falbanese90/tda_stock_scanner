7f50d4b346fd18d05e9c6529735dfb53875ce6b3
Frank Albanese <albanesefc9@gmail.com>
2021-02-18 17:57:52 -0500

    Creates DataFrame and Cleans data

--- stock_scanner.py
+++ stock_scanner.py
@@ -7,13 +7,23 @@ import re
 import os
 
 
+def retrieve_tickers(file):
+    energy_bucket = pd.read_csv(file)
+    energy_bucket = energy_bucket['Symbol']
+    energy_tickers = []
+    for n in energy_bucket:
+        energy_tickers.append(n)
+    tickers = cleanTickers(energy_tickers)
+    return tickers
+
+
 def cleanTickers(energy_tickers):
     tickers = []
     for n in energy_tickers:
         if len(n) <= 4:
             tickers.append(n)
     return tickers
-            
+
 
 def retrieve_tickers(file):
     energy_bucket = pd.read_csv(file)
@@ -24,9 +34,14 @@ def retrieve_tickers(file):
     tickers = cleanTickers(energy_tickers)
     return tickers
 
-tickers = retrieve_tickers('energy_bucket.csv')
 
+# creates a list with all instrument outputs
+with open('instrument_list.pkl', 'rb') as col:
+    columns = pkl.load(col)
+# retieves tickers from selected bucket and cleans them
+tickers = retrieve_tickers('tech_bucket.csv')
 
+# breaks list into sections of 50 loads json into pkl files
 url = 'https://api.tdameritrade.com/v1/instruments'
 start = 0
 end = 50
@@ -48,28 +63,24 @@ while start < len(tickers):
     end += 50
     time.sleep(1)
 
+# loads pkl files and creates a list with contents
 data = []
 for file in files:
     with open(file, 'rb') as f:
         info = pkl.load(f)
     tickers = list(info)
-    points = ['symbol', 'peRatio', 'pegRatio', 'high52']
+    # this is default, every output. Replace with list of
+    # selected parameters from column list to refine
+    points = ['symbol', 'peRatio', 'netProfitMarginTTM', 'vol1DayAvg', 'marketCap']
     for ticker in tickers:
         tick = []
         for point in points:
             tick.append(info[ticker]['fundamental'][point])
         data.append(tick)
+    # removes the pkl file
     os.remove(file)
 
-
-    
-    
-
-
-
-
-
-    
-
-    
-    
+df = pd.DataFrame(data, columns=points)
+for n in points:
+    mask = df[n] != 0
+    df = df[mask]
